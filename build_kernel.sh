c980423ee79086b399c26a6e36718250a85cd9a5
KhasMek <Boushh@gmail.com>
2011-04-08 22:57:45 -0700

    LAST MINUTE ECO2 FIXES     proper location for gcc toolchain     Merge with p1_android_rfs_vzw_defconfig settings     added extra HID sources     fixed build script & added non AOSP script     fixed bad cherry pick for TinyRCU (now fully functional)

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 DATE=$(date +%m%d)
-CONFIG="galaxytab_AOSP"
+CONFIG="galaxytab"
 rm update/kernel_update.zip
 rm update/kernel_update/zImage
 rm ../KERNEL_BACKUPS/"$DATE"_"$CONFIG"_OC.tar
 rm "$DATE"_stdlog_"$CONFIG"_OC.log
 rm "$DATE"_errlog_"$CONFIG"_OC.log
-rm ../KERNEL_BACKUPS/KhasMek.Voodoo.Plus.CDMA.Tab."$DATE".AOSP.OC.zip
-rm ../KERNEL_BACKUPS/KhasMek_"$DATE"_AOSP_OC_zImage
+rm ../KERNEL_BACKUPS/KhasMek.Voodoo.Plus.CDMA.Tab."$DATE".OC.zip
+rm ../KERNEL_BACKUPS/KhasMek_"$DATE"_OC_zImage
 rm ../../../Dropbox/Kernels_etc/Tab/KhasMek.Voodoo.Plus.CDMA.Tab."$DATE".OC.zip
 
 make mrproper
@@ -18,12 +18,12 @@ make -j2 CROSS_COMPILE=../arm-2009q3/bin/arm-none-linux-gnueabi- \
 	ARCH=arm HOSTCFLAGS="-g -O3" 1> "$DATE"_stdlog_"$CONFIG"_OC.log 2> "$DATE"_errlog_"$CONFIG"_OC.log 
 
 
-cp arch/arm/boot/zImage ../KERNEL_BACKUPS/KhasMek_"$DATE"_AOSP_OC_zImage
+cp arch/arm/boot/zImage ../KERNEL_BACKUPS/KhasMek_"$DATE"_OC_zImage
 cp arch/arm/boot/zImage update/kernel_update/zImage
 cd update
 zip -r kernel_update.zip . 
-java -classpath ../../../android-sdk-linux_x86/tools/sign/testsign.jar testsign kernel_update.zip ../../KERNEL_BACKUPS/KhasMek.Voodoo.Plus.CDMA.Tab."$DATE".AOSP.OC.zip
-cp ../../KERNEL_BACKUPS/KhasMek.Voodoo.Plus.CDMA.Tab."$DATE".AOSP.OC.zip ../../../../Dropbox/Kernels_etc/Tab/KhasMek.Voodoo.Plus.CDMA.Tab."$DATE".AOSP.OC.zip
+java -classpath ../../../android-sdk-linux_x86/tools/sign/testsign.jar testsign kernel_update.zip ../../KERNEL_BACKUPS/KhasMek.Voodoo.Plus.CDMA.Tab."$DATE".OC.zip
+cp ../../KERNEL_BACKUPS/KhasMek.Voodoo.Plus.CDMA.Tab."$DATE".OC.zip /media/262CD2492CD213A1/Dropbox/Kernels_etc/Tab/KhasMek.Voodoo.Plus.CDMA.Tab."$DATE".OC.zip
 
 cd kernel_update
 tar --format=ustar -cf kernel_update.tar zImage
